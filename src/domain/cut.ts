696de16b82947049ec4909e94e8e8d4aa2700e81
Alan Gibson <alan.gibson@gmail.com>
2024-11-13 13:43:51 +0100

    Adding lead stub class

--- src/domain/cut.ts
+++ src/domain/cut.ts
@@ -1,13 +1,21 @@
 import { Parent } from "../entity/parent";
 import { PointProperties } from "../geometry/point/point";
 import { Chain } from "./chain";
+import { Lead } from "./lead";
 import { Rapid } from "./rapid";
 
 export class Cut extends Parent<Chain> {
 
   children: Chain[];
   rapidTo: Rapid;
+  leadIn: Lead;
   chain: Chain;
+  leadOut: Lead;
+
+  constructor(chain: Chain) {
+    super([chain]);
+    this.chain = chain;
+  }
 
   get startPoint(): PointProperties {
     // FIXME assumes only one chain, but is array
@@ -17,11 +25,6 @@ export class Cut extends Parent<Chain> {
   get endPoint(): PointProperties {
     // FIXME assumes only one chain, but is array
     return this.children[0].endPoint;
-  }
-
-  constructor(chain: Chain) {
-    super([chain]);
-    this.chain = chain;
   }
 
 }
